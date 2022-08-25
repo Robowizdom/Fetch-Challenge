989a554fe8087f72a0f86ae48524974b7d594e84
Robowizdom <robowizdom@gmail.com>
2022-08-25 10:10:50 +0100

    Fetch challenge done

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    bool send = true;
+    void Start()
+    {
+        InvokeRepeating("Send", 0.5f, 1f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && send)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            send = false;
+        }
+    }
+
+    void Send()
+    {
+        send = true;
+    }
+}
